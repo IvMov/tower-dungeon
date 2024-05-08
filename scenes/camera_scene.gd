77159464b4f4167f01f54331ec7a7c5c353a1bc2
IvanMovchanets <iv.movchanets@gmail.com>
2024-05-08 19:42:31 +0300

    make suzane move much better

--- scenes/camera_scene.gd
+++ scenes/camera_scene.gd
@@ -2,6 +2,10 @@ extends Node3D
 
 @onready var camera_3d = $Camera3D
 
+const SCROLL_POWER: Vector3 = Vector3.ONE/10
+const MAX_ZOOM_OUT: float = 10.0
+const MAX_ZOOM_IN: float = 2
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,13 +14,21 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _unhandled_input(event):
-	if event is InputEventMouseMotion:
-		rotation.y -= event.relative.x / 500	
-		rotation.x -= event.relative.y / 500
-	elif event is InputEventMouseButton:
-		if event.button_index == 4:
-			var direction = camera_3d.global_position - global_position
-			camera_3d.global_position -= Vector3.ONE/10 * direction
-		elif event.button_index == 5:
-			var direction = camera_3d.global_position - global_position
-			camera_3d.global_position += Vector3.ONE/10 * direction
+	if event is InputEventMouseButton:
+		var direction_vector: Vector3 = camera_3d.global_position - global_position
+		var direction_length: float = direction_vector.length();
+		var is_zoom_forward: bool = event.button_index == 4
+		var is_zoom_back: bool = event.button_index == 5
+		
+		if is_zoom_forward && direction_length -  SCROLL_POWER.length() > MAX_ZOOM_IN:
+			zoom_forward(direction_vector)
+		elif is_zoom_back && direction_length +  SCROLL_POWER.length() < MAX_ZOOM_OUT:
+			zoom_back(direction_vector)
+
+
+func zoom_forward(direction_vector: Vector3) -> void:
+		camera_3d.global_position -= SCROLL_POWER * direction_vector
+		
+
+func zoom_back(direction_vector: Vector3) -> void:
+		camera_3d.global_position += SCROLL_POWER * direction_vector
--- scenes/susane.gd
+++ scenes/susane.gd
@@ -1,27 +1,40 @@
 extends CharacterBody3D
 
+@onready var camera_scene = $CameraScene
+@onready var suzanne = $Suzanne
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const SPEED: float = 5.0
+#540 - enogh to jump on 2m wall :) 
+const JUMP_VELOCITY: float = 540.0
+const SENSETIVITY: float = 0.003
+const MAX_ROTATION_TOP: float = 0.6
+const MAX_ROTATION_BOTTOM: float = -0.6
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+func _ready():
+	#Input.mouse_mode = Input.MOUSE_MODE_CONFINED_HIDDEN
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	pass
+	
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
+	if !is_on_floor():
 		velocity.y -= gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("ui_accept") && is_on_floor():
+		velocity.y = JUMP_VELOCITY * delta
+		print(velocity.y)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	suzanne.rotation.y = camera_scene.rotation.y
 	if direction:
+		print(velocity.y)
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
@@ -29,3 +42,11 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * SENSETIVITY)
+		var new_angle = camera_scene.get_rotation().x - event.relative.y * SENSETIVITY;
+		if new_angle < MAX_ROTATION_TOP && new_angle > MAX_ROTATION_BOTTOM:
+			camera_scene.rotation.x = new_angle
+		
