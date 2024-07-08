c394373d40a5e7bf6ebca1507bf55cc1d444871b
IvanMovchanets <iv.movchanets@gmail.com>
2024-07-08 09:49:59 +0300

    random door in random side

--- scenes/maps/map_generator.gd
+++ scenes/maps/map_generator.gd
@@ -4,10 +4,13 @@ class_name MapGenerator extends Node3D
 @export var packed_tunel: PackedScene
 @export var paked_blank_map: PackedScene
 
+const AVAILABILITY_CD: int = 5 # how long steps the oposite tile could not be installed
+const MAX_ROOM_SIZE: int = 16 # must be dividible by CORE_TILE_SIZE
+const MIN_ROOM_SIZE: int = 4 # must be dividible by CORE_TILE_SIZE
+const CORE_TILE_SIZE: int = 4 
 
 var next_x_position: Vector2 = Vector2.ZERO
 var next_y_position: Vector2 = Vector2(0, 2)
-var avaliability_cd: int = 5 # how long steps the oposite tile could not be installed
 
 # core datashelf
 var basic_directions: Array[Vector2] = [Vector2.LEFT, Vector2.DOWN, Vector2.RIGHT, Vector2.UP]
@@ -23,78 +26,23 @@ var map: BlankMap
 var floor_part: Node3D
 var directions: Array[Vector2]
 var current_direction: Vector2
+var exit_position: Vector2
+var room_size: Vector2
 
 
 func _ready():
-	generate_map()
-	#generate_level()
+	
+	#generate_map()
+	generate_level()
 	
 	#map_inst.bake_navigation()
-
-
-# that test method that spawns tiles in random order, to check random algorythmh
-func generate_map() -> void:
-	prepare_blank_map()
-	
-	for i in 20:
-		# handle cd of availability dirrections 
-		for dir in availability:
-			if availability[dir] > 0:
-				availability[dir]-=1
-
-		# handle cd of availability dirrections 
-		directions = basic_directions.duplicate()
-		current_direction = pick_random()
-
-		# add availability cd for new dirrection oposite dir
-		match current_direction:
-			Vector2.LEFT: 
-				availability[Vector2.RIGHT] = avaliability_cd
-			Vector2.RIGHT:
-				availability[Vector2.LEFT] = avaliability_cd
-			Vector2.DOWN:
-				availability[Vector2.UP] = avaliability_cd
-			Vector2.UP: 
-				availability[Vector2.DOWN] = avaliability_cd
-
-		floor_part = packed_floor.instantiate()
-		map.add_tile(floor_part)
-		
-		floor_part.global_position = Vector3(next_x_position.x, 0, next_x_position.y)
-		
-		if randi_range(0, 10) < 1:
-			spawn_additional_room(current_direction)
-			
-		next_x_position += (current_direction*2)
-
-func pick_random() -> Vector2:
-	if directions.is_empty():
-		print("FCK - something wrong! CHECK SPAWNER OF MAP!")
-		return Vector2(0, 0)
-	directions.shuffle()
-	var direction: Vector2 = directions.pop_front()
-	if availability[direction] > 0:
-		return pick_random()
-	return direction
-
-
-func spawn_additional_room(not_allowed_dir: Vector2) -> void:
-	var dead_end_dir: Vector2 = pick_random()
-	while (dead_end_dir == not_allowed_dir):
-		print("I recalculate the dead end dirrection (very low possibility)")
-		dead_end_dir = pick_random()
-
-	floor_part = packed_floor.instantiate()
-	map.add_tile(floor_part)
-	var dead_end_pos = next_x_position + (dead_end_dir*2)
-	floor_part.global_position = Vector3(dead_end_pos.x, 0, dead_end_pos.y)
-
 
 
 func generate_level() -> void:
 	prepare_blank_map()
 	#generate_room(randf_range(4, 10), randf_range(4, 10))
-	generate_room(8, 12)
+	calculate_room_size()
+	generate_room(room_size.x, room_size.y)
 
 
 func generate_room(x: float, y: float) -> void:
@@ -119,23 +67,129 @@ func fill_room_floor(x: float, y: float) -> void:
 		
 
 func add_room_exit() -> void:
+	set_current_dirrection_to_available_random()
+	var door_coordinates: Vector2 = Vector2.ZERO
+	var door_rotation: float = 0.0
+	match current_direction:
+		Vector2.LEFT: 
+			var left_x = next_x_position.x - room_size.x * 2
+			var left_y = calc_rand_y_door_position()
+			print("exit to the LEFT")
+			door_coordinates = Vector2(left_x, left_y)
+			door_rotation = PI/2
+		Vector2.RIGHT:
+			var right_x = next_x_position.x
+			var right_y = calc_rand_y_door_position()
+			print("exit to the RIGHT")
+			door_coordinates = Vector2(right_x, right_y)
+			door_rotation = PI/2
+		Vector2.DOWN:
+			var down_x = calc_rand_x_door_position()
+			var down_y = next_y_position.y - room_size.y * 2
+			print("exit to the DOWN")
+			door_coordinates = Vector2(down_x, down_y)
+		Vector2.UP: 
+			var top_x = calc_rand_x_door_position()
+			var top_y = next_y_position.y
+			print("exit to the UP")
+			door_coordinates = Vector2(top_x, top_y)
+	print(door_coordinates)
 	#choose_room_exit_direction
-	var tuner_position: Vector2 = next_x_position + Vector2(1, calc_rand_y_door_position())
-	var tunel = packed_tunel.instantiate()
+	var tunel: Node3D = packed_tunel.instantiate()
 	map.add_tile(tunel)
-	tunel.global_position = Vector3(tuner_position.x, 0, tuner_position.y)
+	tunel.global_position = Vector3(door_coordinates.x, 0, door_coordinates.y)
+	tunel.rotate_y(door_rotation)
 	print(next_x_position)
 	print(next_y_position)
 
 
 func calc_rand_y_door_position() -> int:
 	var current_y: int = next_y_position.y
-	print(current_y)
 	var possible_locations = (int) (current_y / 4)	
-	print(possible_locations)
+	return (randi_range(0, possible_locations) * 4) + 1
+
+
+func calc_rand_x_door_position() -> int:
+	var current_x: int = next_x_position.x
+	var possible_locations = (int) (current_x / 4)	
 	return (randi_range(0, possible_locations) * 4) + 1
 
 
 func prepare_blank_map() -> void:
 	map = paked_blank_map.instantiate()
 	get_tree().get_first_node_in_group("maps").add_child(map)
+
+
+
+# that test method that spawns tiles in random order, to check random algorythmh
+func generate_map() -> void:
+	prepare_blank_map()
+	
+	for i in 20:
+		set_current_dirrection_to_available_random()
+
+		floor_part = packed_floor.instantiate()
+		map.add_tile(floor_part)
+		
+		floor_part.global_position = Vector3(next_x_position.x, 0, next_x_position.y)
+		
+		if randi_range(0, 10) < 1:
+			spawn_dead_end(current_direction)
+			
+		next_x_position += (current_direction*2)
+
+
+# method encapsulates all updates and other logic related to random pick
+func set_current_dirrection_to_available_random() -> void:
+	update_dirrections_cd()
+	directions = basic_directions.duplicate()
+	current_direction = pick_random()
+	update_available_dirrections()
+
+# add availability cd for new dirrection oposite dir
+func update_available_dirrections() -> void:
+	match current_direction:
+		Vector2.LEFT: 
+			availability[Vector2.RIGHT] = AVAILABILITY_CD
+		Vector2.RIGHT:
+			availability[Vector2.LEFT] = AVAILABILITY_CD
+		Vector2.DOWN:
+			availability[Vector2.UP] = AVAILABILITY_CD
+		Vector2.UP: 
+			availability[Vector2.DOWN] = AVAILABILITY_CD
+
+# handle cd of availability dirrections 
+func update_dirrections_cd() -> void:
+	for dir in availability:
+			if availability[dir] > 0:
+				availability[dir]-=1
+
+
+func pick_random() -> Vector2:
+	if directions.is_empty():
+		print("FCK - something wrong! CHECK SPAWNER OF MAP!")
+		return Vector2(0, 0)
+	directions.shuffle()
+	var direction: Vector2 = directions.pop_front()
+	if availability[direction] > 0:
+		return pick_random()
+	return direction
+
+
+func spawn_dead_end(not_allowed_dir: Vector2) -> void:
+	var dead_end_dir: Vector2 = pick_random()
+	while (dead_end_dir == not_allowed_dir):
+		print("I recalculate the dead end dirrection (very low possibility)")
+		dead_end_dir = pick_random()
+	update_available_dirrections()
+	floor_part = packed_floor.instantiate()
+	map.add_tile(floor_part)
+	var dead_end_pos = next_x_position + (dead_end_dir*2)
+	floor_part.global_position = Vector3(dead_end_pos.x, 0, dead_end_pos.y)
+
+func calculate_room_size() -> void:
+	var variants: int = MAX_ROOM_SIZE / 4
+	var current_x_multiplier: int = randi_range(1, variants)
+	var current_y_multiplier: int = randi_range(1, variants)
+	room_size = Vector2(CORE_TILE_SIZE * current_x_multiplier, CORE_TILE_SIZE * current_y_multiplier)
+	print(room_size)
