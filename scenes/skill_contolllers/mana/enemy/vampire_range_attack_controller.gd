a66b7e657901c7681282488a249659dd0cd23ff8
Erridan <iv.movchanets@gmail.com>
2025-02-08 10:29:13 +0200

    finally vampire avoid collumns when try to shoot me

--- scenes/skill_contolllers/mana/enemy/vampire_range_attack_controller.gd
+++ scenes/skill_contolllers/mana/enemy/vampire_range_attack_controller.gd
@@ -5,7 +5,6 @@ class_name VampireRangeAttackController extends BaseController
 
 @onready var projectiles_box: Node = get_tree().get_first_node_in_group("projectiles")
 
-
 func _ready():
 	skill = Skill.new()
 	skill.base_value = randf_range(3, 5)
@@ -51,8 +50,6 @@ func calc_player_position() -> Vector3:
 
 
 func stop_damage() -> void:
-	print("stopped?")
-	cast_timer.stop()
 	idle_timer.stop()
 	enemy.is_fighting = false
 	enemy.ray_cast_3d.target_position.z = 1
@@ -60,51 +57,41 @@ func stop_damage() -> void:
 
 #used for check is it possible to start damage timer
 func _on_cooldown_timer_timeout():
-	print("cooldown")
 	if enemy.player && enemy.navigation_agent_3d.distance_to_target() <= enemy.battle_distance:
 		do_damage()
-		print("cooldown")
+		cast_timer.start()
+	else:
+		cooldown_timer.start()
+	
+
+
+func _on_cast_timer_timeout() -> void: 
+	if is_idle:
+		idle_timer.start()
+	elif enemy.player && enemy.navigation_agent_3d.distance_to_target() <= enemy.battle_distance:
+		do_damage()
+		cast_timer.start()
+		if idle_timer.is_stopped():
+			idle_timer.start()
+	else: 
+		stop_damage()
+		cooldown_timer.start()
+		
+		
+#used for relocation
+func _on_idle_timer_timeout() -> void:
+	idle_timer.wait_time = randf_range(1, 2)
+	if !is_idle:
 		enemy.ray_cast_3d.target_position.z = 10
-		print(enemy.ray_cast_3d.target_position)
 		if enemy.ray_cast_3d.get_collider():
-			print("collider?")
+			is_idle = true
 			enemy.battle_direction_when_radial = enemy.get_random_sign()
 			enemy.is_fighting = false
 			enemy.is_side_move = true
-		else:
-			if enemy.is_side_move:
-				enemy.battle_direction_when_radial = enemy.get_random_sign()
-				enemy.is_fighting = true
-				enemy.is_side_move = false
-			cooldown_timer.wait_time = 0.5
-		
-		
-	elif enemy.player && enemy.is_fighting && enemy.navigation_agent_3d.distance_to_target() >= enemy.battle_distance:
-		stop_damage()
-		cooldown_timer.wait_time = 1
-	cooldown_timer.start()
-
-
-#used for relocation
-func _on_idle_timer_timeout() -> void:
-
-	#how often vampire change his position in radial
-	idle_timer.wait_time = randf_range(2, 4)
-	print("idle called")
-
-	if enemy.player && enemy.navigation_agent_3d.distance_to_target() <= enemy.battle_distance:
-		idle_timer.start()
-	#print( "z %f" % enemy.ray_cast_3d.target_position.z)
-	#if enemy.is_fighting || enemy.ray_cast_3d.get_collider():
-		#print("if?")
-		#enemy.battle_direction_when_radial = enemy.get_random_sign()
-		#enemy.is_fighting = false
-		#enemy.is_side_move = true
-		#cast_timer.stop()
-	#else:
-		#print("else")
-		##THE PROBLEM IS THAT TOO SHORT MOVE IN SIDE
-		#enemy.is_side_move = false
-		#cast_timer.start()
-	#enemy.ray_cast_3d.target_position.z = 1
-	#idle_timer.start()
+			enemy.ray_cast_3d.target_position.z = 1
+	else:
+		cast_timer.start()
+		is_idle = false
+		enemy.is_fighting = true
+		enemy.is_side_move = false
+	
