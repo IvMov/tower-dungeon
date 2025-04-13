1fa1e5c568e4aad0cf8da8603e594c086e0f4d00
Erridan <iv.movchanets@gmail.com>
2025-04-13 11:52:15 +0300

    add skeleton

---
+++ scenes/enemies/tier_skeleton_enemy.gd
@@ -0,0 +1,88 @@
+class_name TierSkeletonEnemy extends BasicEnemy
+
+@onready var character_skeleton_2: Node3D = $"Body/character-skeleton2"
+
+@onready var dodge_skill_controller: DodgeSkillController = $Body/SkillBox/DodgeSkillController
+@onready var idle_moving_controller: IdleMovingController = $Body/SkillBox/IdleMovingController
+@onready var call_other_enemies_controller: CallOtherEnemiesController = $Body/SkillBox/CallOtherEnemiesController
+@onready var skeleton_range_attack_controller: SkeletonRangeAttackController = $Body/SkillBox/SkeletonRangeAttackController
+
+@onready var body_mesh: MeshInstance3D = $"Body/character-skeleton2/character-skeleton/root/torso"
+
+func _ready():
+	is_ranged = true
+	run_speed = speed * 2
+	agr_radius = 8
+	
+	battle_distance = agr_radius+2
+	
+	
+	chase_player_timer.wait_time = randi_range(2, 5)
+	hp_bar.update(health_component.current_value, health_component.max_value)
+	stamina_bar.update(stamina_component.current_value, stamina_component.max_value)
+	mana_bar.update(mana_component.current_value, mana_component.max_value)
+	#choose_color()
+	skeleton_range_attack_controller.cooldown_timer.start()
+	skeleton_range_attack_controller.skill.base_value *= multiply_characteristics()
+	print("INFO: tier_skeleton instantiated, speed: %s, health: %s, damage %s " % [speed, health_component.current_value, skeleton_range_attack_controller.skill.base_value])
+
+
+func _physics_process(delta: float) -> void:
+	is_runing = false
+	super._physics_process(delta)
+
+func choose_color() -> void:
+	var material = body_mesh.get_surface_override_material(0)
+	material.albedo_color = Color(randf(), randf(), randf())	
+
+
+# targeting and movement
+func detect_target(target_player: Player):
+	is_target_detected = true
+	player = target_player
+	idle_moving_controller.stop_skill()
+	chase_player_timer.stop()
+	expand_agr_area_size()
+	call_other_enemies_controller.use_skill()
+	
+	skeleton_range_attack_controller.damage_player()
+	
+	
+
+func agr_on_player():
+	super.agr_on_player()
+	call_other_enemies_controller.use_skill()
+
+func push_back(player_position: Vector3, push_power: float) -> void:
+	if is_runing:
+		stamina_component.plus(dodge_skill_controller.skill.base_energy_cost)
+		is_runing = false
+	super.push_back(player_position, push_power)
+	
+func lost_target():
+	speed_up_timer.stop()
+	is_runing = false
+	if agr_area.disable_mode: 
+		return
+	is_target_detected = false
+	chase_player_timer.start()
+
+# timeout and area signals handling
+func _on_chase_player_timer_timeout():
+	super._on_chase_player_timer_timeout();
+	if !is_target_detected:
+		speed_up_timer.stop()
+		is_runing = false
+		reset_agr_area_size()
+		idle_moving_controller.use_skill()
+
+
+func _on_speed_up_timer_timeout():
+	if is_fighting:
+		is_runing = false
+		return
+	if player:
+		random_speed()
+		speed_up_timer.start()
+	else:
+		is_runing = false
---
+++ scenes/skill_contolllers/mana/enemy/skeleton_range_attack_controller.gd
@@ -0,0 +1,98 @@
+class_name SkeletonRangeAttackController extends BaseController
+
+@export var enemy: BasicEnemy
+@export var projectile_scene: PackedScene
+
+@onready var projectiles_box: Node = get_tree().get_first_node_in_group("projectiles")
+
+func _ready():
+	skill = Skill.new()
+	skill.base_value = randf_range(3, 5)
+	skill.base_energy_cost = 1
+
+func do_damage() -> float:
+	if enemy.is_dying:
+		return 0
+	enemy.velocity = Vector3.ZERO
+	damage_player()
+	if !enemy.is_fighting:
+		enemy.is_fighting = true
+	return skill.base_value
+
+func damage_player() -> void:
+	var player_position: Vector3 = calc_player_position()
+	if player_position == Vector3.ZERO:
+		return
+
+	
+	if enemy.mana_component.minus(skill.base_energy_cost):
+		player_position.y += 0.5
+		var projectile: SkeletonProjectile = projectile_scene.instantiate()
+		
+		projectiles_box.add_child(projectile)
+		enemy.animation_player.play("attack-melee-right")
+		projectile.damage = skill.base_value if !enemy.is_boss else skill.base_value * 2
+		projectile.push_power = 0
+		projectile.enemy_name = enemy.enemy_name
+		projectile.speed = 10
+		projectile.direction = (player_position - global_position).normalized()
+		if enemy.is_boss:
+			projectile.scale = Vector3.ONE * 2
+		projectile.global_position = global_position
+		
+	else:
+		print("NO STAMINA -_-")
+		# animate - no stamina
+
+func calc_player_position() -> Vector3:
+	if enemy.player:
+		return enemy.player.global_transform.origin
+	return Vector3.ZERO
+
+
+func stop_damage() -> void:
+	idle_timer.stop()
+	enemy.is_fighting = false
+	enemy.ray_cast_3d.target_position.z = 1
+
+
+#used for check is it possible to start damage timer
+func _on_cooldown_timer_timeout():
+	if enemy.player && enemy.navigation_agent_3d.distance_to_target() <= enemy.battle_distance:
+		do_damage()
+		cast_timer.start()
+	else:
+		cooldown_timer.start()
+	
+
+
+func _on_cast_timer_timeout() -> void: 
+	if is_idle:
+		idle_timer.start()
+	elif enemy.player && enemy.navigation_agent_3d.distance_to_target() <= enemy.battle_distance:
+		do_damage()
+		cast_timer.start()
+		if idle_timer.is_stopped():
+			idle_timer.start()
+	else: 
+		stop_damage()
+		cooldown_timer.start()
+		
+		
+#used for relocation
+func _on_idle_timer_timeout() -> void:
+	idle_timer.wait_time = randf_range(1, 2)
+	if !is_idle:
+		enemy.ray_cast_3d.target_position.z = 10
+		if enemy.ray_cast_3d.get_collider():
+			is_idle = true
+			enemy.battle_direction_when_radial = enemy.get_random_sign()
+			enemy.is_fighting = false
+			enemy.is_side_move = true
+			enemy.ray_cast_3d.target_position.z = 1
+	else:
+		cast_timer.start()
+		is_idle = false
+		enemy.is_fighting = true
+		enemy.is_side_move = false
+	
